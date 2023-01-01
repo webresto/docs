685a3fb9b92620ea0015be30a0b825d2a00e3b03
xziy <cziy@ya.ru>
2023-01-01 12:41:53 +0300

    Update ci-publish.sh

--- ci-publish.sh
+++ ci-publish.sh
@@ -4,12 +4,12 @@ set -x;
 SOURCE=$1 
 TARGET=$2 
 COMMIT=$(git log -1 --pretty=%B | tr -d '\n')
-
+FROM=$(basename $PWD)
 # check updates
 if [ ! -z $(git diff-tree --no-commit-id --name-only -r $(git log -1 --pretty=%H) | grep $SOURCE) ] 
 then 
  curl -i -X PUT -H 'Authorization: token '${GITHUB_DOCS_BOT_TOKEN} -d "{\"path\": \"$TARGET\", \
-   \"message\": \"from fbuilder: ${COMMIT}\", \"content\":  \"$(openssl base64 -A -in $SOURCE)\", \"branch\": \"master\",\
+   \"message\": \"from ${FROM}: ${COMMIT}\", \"content\":  \"$(openssl base64 -A -in $SOURCE)\", \"branch\": \"master\",\
     \"sha\": $(curl -X GET https://api.github.com/repos/webresto/docs/contents/$TARGET | jq .sha)}" \
     https://api.github.com/repos/webresto/docs/contents/$TARGET
 else
